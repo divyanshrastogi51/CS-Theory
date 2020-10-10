45adc29aca007fa8b8095a869d2c611b80098dac
Divyansh Rastogi <54467001+divyanshrastogi51@users.noreply.github.com>
2020-10-10 21:34:04 +0530

    Update schema.sql

--- mysql_proj/schema.sql
+++ mysql_proj/schema.sql
@@ -6,6 +6,3 @@ CREATE TABLE users
     email VARCHAR(255) ,
 	created_at TIMESTAMP DEFAULT NOW()
 );
-INSERT INTO users (email) VALUES('Katiedsfsd34@yahoo.com'), ('Tunsdfsdsdgsdde@gmail.com');
-select * from users;
-DELETE FROM users WHERE id > 25;
